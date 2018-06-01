958326de0c6227e3fc8b4b803fb73714bb2a9685
condret <condr3t@protonmail.com>
2018-06-01 03:47:45 +0000

    implement set_bcache_num

--- libr/io/bcache.c
+++ libr/io/bcache.c
@@ -6,6 +6,24 @@
 #define	RIODBCE	RIODescBCacheElement
 #define	RIODBC	RIODescBCache
 
+static RIODBC *_riodbc_new(RIO *io) {
+	RIODBC *bc = R_NEW0(RIODBC);
+	if(bc) {
+		bc->caches = R_NEW0(io->block_cache_num, RIODBCE *);
+		if (!bc->caches) {
+			free (bc);
+			return NULL;
+		}
+		bc->bce = dict_new (sizeof(ut64), NULL);
+		if (!bc->bce) {
+			free (bc->caches);
+			free (bc);
+			return NULL;
+		}
+	}
+	return bc;
+}
+
 static RIODBCE *_riodbce_new(int size) {
 	RIODBCE *ret = R_NEW(RIODBCE);
 
@@ -37,8 +55,8 @@ static void _riodbc_remove_idx(RIODBC *bcache, ut32 idx) {
 static void _riodbc_remove_all(RIODesc *desc) {
 	ut32 i;
 
-	if(desc->bcache) {
-		for(i = 0; i < desc->io->block_cache_num; i++) {
+	if (desc->bcache) {
+		for (i = 0; i < desc->io->block_cache_num; i++) {
 			_riodbc_remove_idx (desc->bcache, i);
 		}
 		desc->bcache->base_idx = 0;
@@ -48,8 +66,8 @@ static void _riodbc_remove_all(RIODesc *desc) {
 static bool desc_set_block_cache_size_cb(void *user, void *data, ut32 id) {
 	RIODesc *desc = (RIODesc *)data;
 
-	if(desc->plugin && desc->plugin->cacheable) {
-		_riodbc_remove_all(desc);
+	if (desc->plugin && desc->plugin->cacheable) {
+		_riodbc_remove_all (desc);
 	}
 	return true;
 }
@@ -57,11 +75,11 @@ static bool desc_set_block_cache_size_cb(void *user, void *data, ut32 id) {
 R_API void r_io_set_block_cache_size(RIO *io, ut32 size) {
 	if(io) {
 		// anything below 8 seems not to be a reasonable size
-		if((size < 9) || (io->block_cache_size == size)) {
+		if ((size < 9) || (io->block_cache_size == size)) {
 			return;
 		}
-		if(io->files) {
-			r_id_storage_foreach(io->files, desc_set_block_cache_size_cb, NULL);
+		if (io->files) {
+			r_id_storage_foreach (io->files, desc_set_block_cache_size_cb, NULL);
 		}
 		io->block_cache_size = size;
 	}
@@ -70,15 +88,44 @@ R_API void r_io_set_block_cache_size(RIO *io, ut32 size) {
 static bool desc_set_block_cache_num_bigger_cb(void *user, void *data, ut32 id) {
 	RIODesc *desc = (RIODesc *)data;
 	ut32 *num = (ut32 *)user;
-	RIODBC **ptr = NULL;
+	RIODBCE **ptr = NULL;
 
-	if(desc->plugin && desc->plugin->cacheable) {
-		ptr = realloc(desc->bcache->caches, sizeof(RIODBC *) * num[0]);
-		if(!ptr) {
+	if (desc->plugin && desc->plugin->cacheable) {
+		ptr = realloc (desc->bcache->caches, sizeof(RIODBCE *) * num[0]);
+		if (!ptr) {
 			return false;
 		}
 		desc->bcache->caches = ptr;
 	}
+	return true;
+}
+
+//copy elements that are more likely to be needed
+static bool desc_set_block_cache_num_smaller_cb(void *user, void *data, ut32 id) {
+	RIODesc *desc = (RIODesc *)data;
+	ut32 i, j, *num = (ut32 *)user;
+	RIODBCE **new_caches = R_NEWS0(num[0], RIODBCE *);
+
+	if (!new_caches) {
+		return false;
+	}
+
+	j = desc->bcache->base_idx;
+	for (i = 0; i < num[0]; i++) {
+		new_caches[i] = desc->bcache->caches[j];
+		new_caches[i]->real_idx = i;
+		j = _inc_ring_idx (j, num[0]);
+	}
+	while (j != desc->bcache->base_idx) {
+		if (desc->bcache->caches[j]) {
+			dict_del (desc->bcache->bce,
+					desc->bcache->caches[j]->bce_addr);
+			_riodbce_free (desc->bcache->caches[j]);
+		}
+		j = _inc_ring_idx (j, num[0]);
+	}
+	free (desc->bcache->caches);
+	desc->bcache->caches = new_caches;
 	return true;
 }
 
@@ -88,7 +135,11 @@ R_API void r_io_set_block_cache_num(RIO *io, ut32 num) {
 			return;
 		}
 		if(io->files) {
-			r_id_storage_foreach(io->files, desc_set_block_cache_num_cb, &num);
+			if (num > io->block_cache_num) {
+				r_id_storage_foreach (io->files, desc_set_block_cache_num_bigger_cb, &num);
+			} else {
+				r_id_storage_foreach (io->files, desc_set_block_cache_num_smaller_cb, &num);
+			}
 		}
 		io->block_cache_num = num;
 	}
@@ -131,6 +182,7 @@ beach:
 
 static RIODBCE *_next_block(RIODesc *desc, RIODBCE *bce) {
 	if (desc->bcache->base_idx == bce->real_idx) {
+// in this case bce is already on top
 		return NULL;
 	}
 	return desc->bcache->caches[_dec_ring_idx(bce->real_idx,
@@ -148,12 +200,8 @@ static void _xchg_blocks(RIODBC *bc, RIODBCE *bce0, RIODBCE *bce1) {
 /* fills block with data from file at block_addr, assumes block_addr is in file */
 static int _fill_block(RIODesc *desc, RIODBCE *bce) {
 	int len = desc->io->block_cache_size;
-	if (!desc->plugin || !desc->plugin->lseek || !desc->plugin->read) {
-		return -1;
-	}
 //	if ()	//sanity checks here
-	desc->plugin->lseek (desc->io, desc, bce->bce_addr, R_IO_SEEK_SET);
-	return desc->plugin->read (desc->io, desc, bce->data, len);
+	return r_io_plugin_read_at (desc, bce->bce_addr, bce->data, len);
 }
 
 /* gets usable block at addr (block-addr)*/
@@ -164,8 +212,12 @@ static RIODBCE *_get_block_at(RIODesc *desc, ut64 addr) {
 	if(!ret) {
 		ret = _allocate_or_sacrifice_for_block_at(desc, addr);
 		if(ret) {
-			_fill_block(desc, ret);
-			//check return value here pls
+			if(_fill_block(desc, ret) < 0) {
+				dict_del(desc->bcache->bce, ret->bce_addr);
+				desc->bcache->base_idx = _inc_ring_idx(desc->bcache->base_idx, 
+						desc->io->block_cache_num);
+				ret = NULL;
+			}
 		}
 		goto beach;
 	}
@@ -187,13 +239,13 @@ static int _read_from_single_block (RIODesc *desc, ut64 addr, ut8 *buf, int len)
 	ut64 block_addr = _get_block_addr(desc->io, addr);
 
 	bce = _get_block_at(desc, block_addr);
-	if(!bce) {	//we should try to read from plugin in this case
-		return 0;
-	}
 	if(io->block_cache_size < len) {
 		len = io->block_cache_size;
 	}
 	len -= (block_addr - addr);
+	if(!bce) {
+		return r_io_plugin_read_at (desc, block_addr, buf, len);
+	}
 	memcpy (buf, bce->data, len);
 	return len;
 }
