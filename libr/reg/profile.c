2f06520b08570ff2ea512f3ebc0134ea2416c476
condret <condr3t@protonmail.com>
2024-12-14 18:46:26 +0100

    Remove RArchConfig from RReg

--- libr/reg/profile.c
+++ libr/reg/profile.c
@@ -242,22 +242,17 @@ R_API bool r_reg_set_profile_string(RReg *reg, const char *str) {
 			} else {
 				const char *r = NULL;
 				if (*first == '^') {
-					int endian = R_SYS_ENDIAN;
+					reg->endian = R_SYS_ENDIAN;
 					switch (first[1]) {
 					case 'l':
-						endian = R_SYS_ENDIAN_LITTLE;
+						reg->endian = R_SYS_ENDIAN_LITTLE;
 						break;
 					case 'b':
-						endian = R_SYS_ENDIAN_BIG;
+						reg->endian = R_SYS_ENDIAN_BIG;
 						break;
 					case 'm':
-						endian = R_SYS_ENDIAN_MIDDLE;
+						reg->endian = R_SYS_ENDIAN_MIDDLE;
 						break;
-					}
-					if (reg->config) {
-						reg->config->endian = endian;
-					} else {
-						R_LOG_WARN ("Cannot force reg profile endianness");
 					}
 				} else if (*first == '=') {
 					r = parse_alias (reg, tok, j);
